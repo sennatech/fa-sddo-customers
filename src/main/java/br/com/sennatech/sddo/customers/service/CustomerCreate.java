052709d9998b99709c86f5539ed50a5edb25cc9e
Leonardo Bezerra <leonardo.bispo@sennatech.com>
2023-09-05 17:59:34 -0300

    tryouts fix of the fix #1

--- src/main/java/br/com/sennatech/sddo/customers/service/CustomerCreate.java
+++ src/main/java/br/com/sennatech/sddo/customers/service/CustomerCreate.java
@@ -1,12 +1,13 @@
 package br.com.sennatech.sddo.customers.service;
 
 import java.util.function.Supplier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomerCreate implements Supplier<String> {
 
   @Override
   public String get() {
     return "hello world";
   }
-  
 }
