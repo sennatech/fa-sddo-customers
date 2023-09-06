a1f79a8097d84d8cce15df726c8c9b42439c8239
Leonardo Bezerra <leonardo.bispo@sennatech.com>
2023-09-06 15:59:28 -0300

    fix tryout

--- src/main/java/br/com/sennatech/sddo/customers/handler/CustomerHandler.java
+++ src/main/java/br/com/sennatech/sddo/customers/handler/CustomerHandler.java
@@ -1,12 +1,12 @@
 package br.com.sennatech.sddo.customers.handler;
 
 import br.com.sennatech.sddo.customers.domain.CustomerDTO;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -14,18 +14,24 @@ import java.util.Optional;
 @Component
 public class CustomerHandler {
 
-
-
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @FunctionName("customerCreate")
     public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = { HttpMethod.GET,
-                    HttpMethod.POST }, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<CustomerDTO>> request,
+            @HttpTrigger(name = "req", 
+                methods = {HttpMethod.POST}, 
+                authLevel = AuthorizationLevel.FUNCTION,
+                route = "customers") HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
-        var customer = request.getBody().get();
-        context.getLogger().info(customer.toString());
-        return request.createResponseBuilder(HttpStatus.OK).body(customer).build();
+        try {
+            var customer = mapper.readValue(request.getBody().get(), CustomerDTO.class);
+            context.getLogger().info(customer.toString());
+            return request.createResponseBuilder(HttpStatus.OK).body(customer).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
 
