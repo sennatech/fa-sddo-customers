cae6fa34378796a738f4fb35d84352c6dc3a32b5
Vanessa Fernanda Costa Won Ancken Barretos <vanessa.ancken@sennatech.com.br>
2023-09-09 22:01:19 -0300

    teste

---
+++ src/main/java/br/com/sennatech/sddo/customers/handler/GetCustomersHandler.java
@@ -0,0 +1,47 @@
+package br.com.sennatech.sddo.customers.handler;
+
+import br.com.sennatech.sddo.customers.domain.dto.Error;
+import br.com.sennatech.sddo.customers.service.GetCustomer;
+import br.com.sennatech.sddo.customers.service.GetCustomers;
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class GetCustomersHandler {
+
+
+    private final GetCustomers getCustomers;
+
+    @FunctionName("getCustomers")
+    public HttpResponseMessage getCustomer(
+            @HttpTrigger(
+                    name = "req",
+                    methods = {HttpMethod.GET},
+                    authLevel = AuthorizationLevel.ANONYMOUS
+            ) HttpRequestMessage request,
+            ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+        try{
+            return request
+                    .createResponseBuilder(HttpStatus.CREATED)
+                    .body(getCustomers.execute())
+                    .build();
+        } catch (Exception e){
+            return request
+                    .createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Error.builder().message(e.getMessage()).build())
+                    .build();
+        }
+    }
+}
+
+
+
+
+
