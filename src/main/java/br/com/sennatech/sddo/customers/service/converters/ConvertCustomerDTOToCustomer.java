107ee92bfc48d9e655f46cde470b08a201aae273
Leonardo Bezerra <leonardo.bispo@sennatech.com>
2023-10-11 10:47:36 -0300

    change pom.xml

--- src/main/java/br/com/sennatech/sddo/customers/service/converters/ConvertCustomerDTOToCustomer.java
+++ src/main/java/br/com/sennatech/sddo/customers/service/converters/ConvertCustomerDTOToCustomer.java
@@ -15,8 +15,6 @@ public class ConvertCustomerDTOToCustomer {
     private final ModelMapper mapper;
 
     public Customer convert(CustomerDTO request){
-
-
         final var customer = mapper.map(request, Customer.class);
         customer.setBirthdate( LocalDate.parse(request.getBirthdate()));
         return customer;
